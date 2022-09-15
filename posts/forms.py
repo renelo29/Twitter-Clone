d59bbf3fc2da6084a781299f931768dfe39c3da8
rene <renswebdev@gmail.com>
2022-09-14 20:43:44 -0700

    pushing the final product

---
+++ posts/forms.py
@@ -0,0 +1,10 @@
+from dataclasses import fields
+from pyexpat import model
+from django import forms
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+      class Meta:
+            model = Post
+            fields = '__all__'
